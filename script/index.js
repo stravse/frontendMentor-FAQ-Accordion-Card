dd8f6352a260f93c63ad9348178124ce7ab00033
Ace <stravse@gmail.com>
2022-05-17 11:14:37 +0800

    finished mobile and desktop view

--- script/index.js
+++ script/index.js
@@ -4,15 +4,17 @@ const box = document.querySelector(".box-image");
 
 questions.forEach(question => {
     question.addEventListener("click", function(){
-        
-        // close all other open tab if there is any
-        if(!question.classList.contains("active")){
-            questions.forEach(function(question){
-                question.classList.remove("active");
+        // close all other open tab if clicking on a non active tab
+        if(!this.classList.contains("active")){
+            questions.forEach( quest => {
+                quest.classList.contains("active");{
+                    quest.classList.remove("active");
+                    box.classList.remove("move-box");
+                }
             })
         }
-        
         // open or close clicked tab 
         this.classList.toggle("active");
+        box.classList.toggle("move-box");
     })
 });
